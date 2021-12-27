f44089f2dc5c950bd2a7aab7c5457f8638c4b0fd
Luciano Ferreira <luciano2023silva@gmail.com>
2021-12-27 10:52:47 -0300

    fix: fixing pagination bulklets from Slider

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,13 +1,15 @@
 import { Flex, Heading, Text } from '@chakra-ui/react';
 import Link from 'next/link';
-import { Navigation, Pagination, Scrollbar } from 'swiper';
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Autoplay]);
+
 
 export default function Slider() {
-  return(
-    <Flex w='100%' maxW='1240px' mx='auto' mb={['5', '10']} h={['250px', '450px']}> 
+  return (
+    <Flex w='100%' maxW='1240px' mx='auto' mb={['5', '10']} h={['250px', '450px']}>
       <Swiper
         slidesPerView={1}
         navigation
@@ -15,12 +17,30 @@ export default function Slider() {
         autoplay={{
           delay: 4000,
         }}
-        style={{
-          width: '100%',
-          flex: '1'
-        }}
+        style={{ width: '100%', flex: '1' }}
       >
-        <SwiperSlide> 
+        <SwiperSlide>
+          <Flex
+            w="100%"
+            h="100%"
+            align="center"
+            justify="center"
+            direction="column"
+            bgImage="url(/images/europe.png)"
+            bgPosition="100% 30%"
+            bgRepeat="no-repeat"
+            bgSize="cover"
+            textAlign="center"
+          >
+            <Link href={`/continent`}>
+              <a>
+                <Heading fontSize={["3xl", "4xl", "5xl"]} color="gray.100" fontWeight="bold">China</Heading>
+                <Text fontWeight="bold" color="gray.300" fontSize={["0.8rem", "1xl", "2xl"]} mt={["2", "4"]}>O continente do ipman</Text>
+              </a>
+            </Link>
+          </Flex>
+        </SwiperSlide>
+        <SwiperSlide>
           <Flex
             w='100%'
             h='100%'
@@ -41,28 +61,6 @@ export default function Slider() {
             </Link>
           </Flex>
         </SwiperSlide>
-        <SwiperSlide> 
-          <Flex
-            w='100%'
-            h='100%'
-            align='center'
-            justify='center'
-            direction='column'
-            bgImage="url(/images/europe.png)"
-            bgRepeat='no-repeat'
-            bgSize='cover'
-            textAlign='center'
-            bgPosition='center'
-          >
-            <Link href='/continent/europe'>
-              <a>
-                <Heading fontSize={['3xl', '4xl', '5xl']} color='gray.100' fontWeight='bold' >Europa</Heading>
-                <Text fontWeight='bold' color='gray.300' fontSize={['0.8rem', '1xl', '2xl']} mt={['2', '4']} >O continente mais antigo</Text>
-              </a>
-            </Link>
-          </Flex>
-        </SwiperSlide>
-        
       </Swiper>
     </Flex>
   )
